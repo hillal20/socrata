0db6f6438ad9bfad3d6246fafdb89aa5de60d0ef
hilal aissani <hilalaissani@gmail.com>
2019-01-10 18:21:32 -0500

    socrata code challenge

--- plateau.js
+++ plateau.js
@@ -4,35 +4,23 @@ class Rover {
     this.heading = heading;
   }
 
-  /**
-   * Rotate to given direction. L or R.
-   */
-  rotate(to) {
-    this.heading = Rover.rotateMap[this.heading][to];
+  turnTo(to) {
+    this.heading = Rover.turns[this.heading][to];
   }
 
-  /**
-   * Move one step forward.
-   */
-  move(predicate) {
-    const moveBy = Rover.moveMap[this.heading],
+  moveTo(predicate) {
+    const moveBy = Rover.move[this.heading],
       nextPos = { x: this.pos.x, y: this.pos.y };
 
-    // Set next pos.
-    nextPos[moveBy.axis] += moveBy.amount;
+    nextPos[moveBy.axis] += moveBy.steps;
 
-    // Check if move is legal.
     const isLegal = predicate(nextPos);
 
-    // Update position if move is legal.
     if (isLegal) {
       this.pos = nextPos;
     }
   }
 
-  /**
-   * Get current state.
-   */
   getState() {
     return {
       pos: this.pos,
@@ -41,70 +29,42 @@ class Rover {
   }
 }
 
-/**
- * Rotate map by compass directions.
- */
-Rover.rotateMap = {
+Rover.turns = {
   N: { L: "W", R: "E" },
   E: { L: "N", R: "S" },
   S: { L: "E", R: "W" },
   W: { L: "S", R: "N" }
 };
 
-/**
- * Move map by compass directions.
- */
-Rover.moveMap = {
-  N: { axis: "y", amount: +1 },
-  S: { axis: "y", amount: -1 },
-  E: { axis: "x", amount: +1 },
-  W: { axis: "x", amount: -1 }
+Rover.move = {
+  N: { axis: "y", steps: +1 },
+  S: { axis: "y", steps: -1 },
+  E: { axis: "x", steps: +1 },
+  W: { axis: "x", steps: -1 }
 };
-
-/**
- * Export Rover.
- */
-/**
- * Get dependencies.
- */
-
-/**
- * Manager class for Rovers.
- */
-class MarsRover {
-  /**
-   * Create initial data.
-   */
-  constructor(xSize, ySize) {
+///////////////////////////////////////////////////////////
+class Plateau {
+  constructor(maxX, maxY) {
     this.size = {
-      x: xSize,
-      y: ySize
+      x: maxX,
+      y: maxY
     };
 
     this.rovers = [];
-    this.activeRover = null;
+    this.currentRover = null;
 
-    this.isPositionAvailable = this.isPositionAvailable.bind(this);
+    this.availableP = this.availableP.bind(this);
     this.executeCommand = this.executeCommand.bind(this);
   }
 
-  /**
-   * Check for plateau borders and rover collisions.
-   */
-  isPositionAvailable(nextPos) {
-    return this.isPositionExist(nextPos) && !this.getRoverByPosition(nextPos);
+  availableP(nextPos) {
+    return this.truePosition(nextPos) && !this.getRoverByPosition(nextPos);
   }
 
-  /**
-   * Check if given coordinates exist on plateau.
-   */
-  isPositionExist({ x, y }) {
+  truePosition({ x, y }) {
     return x > -1 && y > -1 && x <= this.size.x && y <= this.size.y;
   }
 
-  /**
-   *  Get rover which on given position.
-   */
   getRoverByPosition({ x, y }) {
     return this.rovers.find(rover => {
       const state = rover.getState();
@@ -113,63 +73,43 @@ class MarsRover {
     });
   }
 
-  /**
-   * Add and activate rover.
-   */
-
   addRover(x, y, heading) {
-    this.activeRover = null;
+    this.currentRover = null;
 
-    // Dont add rover over another one.
-    if (!this.isPositionAvailable({ x, y })) {
+    if (!this.availableP({ x, y })) {
       return false;
     }
 
-    // Create rover.
     const rover = new Rover(x, y, heading);
 
     this.rovers.push(rover);
-    this.activeRover = rover;
+    this.currentRover = rover;
 
-    // This index number acts like an id,
-    // and can be used to activate this rover later.
     return this.rovers.length - 1;
   }
 
-  /**
-   * Activate rover with given index number.
-   */
   activateRover(index) {
-    return !!(this.activeRover = this.rovers[index]);
+    return !!(this.currentRover = this.rovers[index]);
   }
 
-  /**
-   * Execute single command.
-   */
   executeCommand(command) {
-    if (!this.activeRover) {
+    if (!this.currentRover) {
       return false;
     }
 
     if (command === "M") {
-      return this.activeRover.move(this.isPositionAvailable);
+      return this.currentRover.moveTo(this.availableP);
     }
 
     if (command === "L" || command === "R") {
-      return this.activeRover.rotate(command);
+      return this.currentRover.turnTo(command);
     }
   }
 
-  /**
-   * Distribute command to active rover.
-   */
   sendCommand(commandList) {
     [...commandList].forEach(this.executeCommand);
   }
 
-  /**
-   * Simply create array of rover position summaries.
-   */
   getFinalPositions() {
     return this.rovers.map(rover => {
       const state = rover.getState();
@@ -179,15 +119,12 @@ class MarsRover {
   }
 }
 
-/**
- * Export MarsRover.
- */
-const marsRover = new MarsRover(5, 5);
+const newPlateau = new Plateau(5, 5);
 
-marsRover.addRover(1, 2, "N");
-marsRover.sendCommand("LMLMLMLMM");
+newPlateau.addRover(1, 2, "N");
+newPlateau.sendCommand("LMLMLMLMM");
 
-marsRover.addRover(3, 3, "E");
-marsRover.sendCommand("MMRMMRMRRM");
+newPlateau.addRover(3, 3, "E");
+newPlateau.sendCommand("MMRMMRMRRM");
 
-console.log(marsRover.getFinalPositions());
+console.log(newPlateau.getFinalPositions());
